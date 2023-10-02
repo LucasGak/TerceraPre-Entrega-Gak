81477eadac8efb39fad4c4330c6f465e161af055
lucas.gak <lucasgak@gmail.com>
2023-10-02 17:59:38 -0300

    Funcionaaa

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -4,9 +4,9 @@ from .views import *
 
 urlpatterns = [
     path('', views.inicio),
-    path('cursos', views.cursos, name="Cursos"),
-    path('profesores', views.profesores),
-    path('estudiantes', views.estudiantes),
-    path('entregables', views.entregables),
+    path('cursos/', cursos, name="cursos"),
+    path('profesores/', profesores, name="profesores"),
+    path('estudiantes/', estudiantes, name="estudiantes"),
+    path('entregables/', entregables, name="entregables"),
 
 ]
