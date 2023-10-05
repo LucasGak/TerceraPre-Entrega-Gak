3ee1e710089768757044ef1fe937e7bdf1358da7
lucas.gak <lucasgak@gmail.com>
2023-10-05 14:58:50 -0300

    Tercera Pre-Entrega

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from AppCoder import views
 from .views import *
 
-
 urlpatterns = [
     path('', views.inicio, name="inicio"),
     path('cursos/', cursos, name="cursos"),
